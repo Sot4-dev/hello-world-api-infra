d59e1c0d98a28c7bdfed543cfecd78a8dc6718e0
Sot4 <Sot4.develop@gmail.com>
2025-10-06 22:54:06 +0000

    修正：aws_instance.app_serverのamiを修正

--- ec2.tf
+++ ec2.tf
@@ -41,8 +41,8 @@ data "aws_ami" "ecs_optimized_linux_2" {
     }
 }
 
-resource "aws_instance" "app.server" {
-  ami = data.aws_ami.amazon_linux_2.id
+resource "aws_instance" "app_server" {
+  ami = data.aws_ami.ecs_optimized_linux_2.id
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
