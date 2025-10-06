81688628455e9028ca651bd815f17e40eb67eac9
Sot4 <Sot4.develop@gmail.com>
2025-10-06 23:08:23 +0000

    追加：リージョン設定変数を作成

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "aws_account_id" {
   type = string
   description = "The AWS Account ID"
 }
+
+variable "primary_region" {
+  type = string
+  description = "The primary AWS region"
+  default = "ap-northeast-1"
+}
