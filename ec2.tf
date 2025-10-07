c13de141b93ccc668e1a73d87aed853ae4d14e6b
Sot4 <Sot4.develop@gmail.com>
2025-10-07 10:10:35 +0000

    追加：userdataを記述

--- ec2.tf
+++ ec2.tf
@@ -48,6 +48,12 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   key_name = "terraform-key"
 
+  user_data = <<-EOF
+              #!/bin/bash
+
+              aws ecr get-login-password -region ${var.primary_region} | docker login --username AWS password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.primary_region}.amazonaws.com
+              docker run -d -p 80:5000 ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.primary_region}.amazonaws.com/hello-world-api:latest
+              EOF
   tags = {
     Name = "hello-world-app-serever"
   }
