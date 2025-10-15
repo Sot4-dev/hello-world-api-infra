f816f57f979c11cee13111d11423e82f4136a09e
Sot4 <Sot4.develop@gmail.com>
2025-10-15 20:11:00 +0900

    修正：ecs_optimized_amiのパスを修正

--- ec2.tf
+++ ec2.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "app_sg" {
 }
 
 data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended/image_id"
 }
 
 resource "aws_instance" "app_server" {
