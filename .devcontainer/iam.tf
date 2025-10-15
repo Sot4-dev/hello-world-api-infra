5a3f5d7daf4b40dbfc71bbd89ee0afd382b55b91
Sot4 <Sot4.develop@gmail.com>
2025-10-15 09:51:01 +0000

    追加：EC2にECR参照権限を追加

---
+++ .devcontainer/iam.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    actions = [ "sts:AssumeRole" ]
+
+    principals {
+      type = "Service"
+      identifiers = [ "ec2.amazonaws.com" ]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2-ecr-pull-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy
+
+  tags = {
+    Name = "EC2 ECR Pull Role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ecr_policy_attachment" {
+    role = aws_iam_role.ec2_role
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-ecr-pull-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
