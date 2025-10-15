3154b1968cb74ea27398180065e728ad45397dea
Sot4 <Sot4.develop@gmail.com>
2025-10-15 19:05:53 +0900

    修正：パラメータのわたし方を修正

--- iam.tf
+++ iam.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ec2_ecr_policy_attachment" {
-    role = aws_iam_role.ec2_role
+    role = aws_iam_role.ec2_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
